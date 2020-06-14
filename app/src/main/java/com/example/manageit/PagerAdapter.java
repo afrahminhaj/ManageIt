b98e71521bab83c3e16b31b11b7338fc5d606521
SLS Harshitha <59821143+harshitha008@users.noreply.github.com>
2020-06-14 14:55:36 +0530

    Update PagerAdapter.java

--- app/src/main/java/com/example/manageit/PagerAdapter.java
+++ app/src/main/java/com/example/manageit/PagerAdapter.java
@@ -3,8 +3,10 @@ package com.example.manageit;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
-public class PagerAdapter extends FragmentPagerAdapter {
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
 
     private int numOfTabs;
 
